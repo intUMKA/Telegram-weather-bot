efbd1a752f8198c364d48cf12c0a2bba962672af
Ivan Todorashko <vansarik07@gmail.com>
2023-09-09 21:51:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import telebot
 import requests
 import json
-#import sqlite3
-#from telebot import types
+
 
 bot = telebot.TeleBot('6637484481:AAFWFD0qftAqF9DklO7-8AfPjgi1KxgoUMk')
 API = '6433f82a0235dcb9df58055f36b63479'
@@ -29,6 +28,7 @@ def get_weather(message):
     else:
         bot.reply_to(message, f'That`s a dog shit, try to enter a real city name')
 
+
 bot.polling(none_stop=True)
 
 
@@ -36,77 +36,3 @@ bot.polling(none_stop=True)
 
 
 
-
-
-
-
-
-
-
-#name = None
-
-
-
-
-"""cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, name varchar(50), pass varchar(50))')
-    conn.commit()
-    cur.close()
-    conn.close()  
-
-
-   bot.send_message(message.chat.id, 'Hi, let`s registrate you! Please enter your name:')
-    bot.register_next_step_handler(message, user_name)
-
-def user_name(message):
-    global name
-    name = message.text.strip()
-    bot.send_message(message.chat.id, ' Please enter your password:')
-    bot.register_next_step_handler(message, user_pass)
-
-def user_pass(message):
-    password = message.text.strip()
-
-    conn = sqlite3.connect('telebot.sql')
-    cur = conn.cursor()
-
-    cur.execute("INSERT INTO users (name, pass) VALUES ('%s', '%s') " % (name, password))
-    conn.commit()
-    cur.close()
-    conn.close()
-
-    markup = telebot.types.InlineKeyboardMarkup()
-    markup.add(telebot.types.InlineKeyboardButton('List of users', callback_data='users'))
-    bot.send_message(message.chat.id, 'You`ve been registrated!', reply_markup=markup)
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    conn = sqlite3.connect('telebot.sql')
-    cur = conn.cursor()
-
-    cur.execute('SELECT * FROM users')
-    users = cur.fetchall()
-
-    info = ''
-    for el in users:
-        info += f'Name: {el[1]}, password: {el[2]}\n'
-    cur.close()
-    conn.close()
-
-    bot.send_message(call.message.chat.id, info)
-"""
-
-
-
-
-
-#@bot.message_handler(commands=['start'])
-#def main(message):
-#bot.send_message(message.chat.id, 'Meow!')
-
-
-#photo = open('C:\Users\Ivan Todorashko\Pictures\Saved Pictures\umka.jpg', 'rb')
-
-
-#@bot.message_handler(commands=['pic'])
-#def main(message):
-#    bot.send_photo(message.chat.id, photo)
